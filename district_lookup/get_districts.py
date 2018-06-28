eede11efcbe7a22f87acacec19fb1e5351e7d568
jackiekazil <jackiekazil@gmail.com>
2018-06-28 17:27:30 -0400

    General update - added file explaination text.

--- district_lookup/get_districts.py
+++ district_lookup/get_districts.py
@@ -1,16 +1,29 @@
 import csv
 import logging
+import os
 import requests
+import time
 
 from config import API_KEY
 from datetime import datetime
-from os import mkdir
 
 
 output_folder = '../../signature_output/'
 # source_data = '../../data/kft-signers_sample.csv'
 source_data = '../../data/kft-signers.csv'
-logging_level = logging.DEBUG
+logging_level = logging.INFO
+error_count = 0
+
+header_text = 'The following individuals signed the Lawyers Moms of America \
+open letter dated June 29, 2018, which demands a just and humane resolution \
+to the ongoing crisis of families seeking asylum in the U.S. being separated \
+or detained at the border. To read the letter, visit: \
+https://lawyermomsofamerica.squarespace.com/openletter/\n\
+\n\
+\n\
+Signers located in your district\n\
+----------------------------------\n\
+'
 
 
 def read_data(csv_file):
@@ -35,19 +48,17 @@ def get_address(rec):
 
 
 def get_rep_rec(address, filters):
+    """
+
+    Returns http response object.
+
+    """
 
     url_base = 'https://www.googleapis.com/civicinfo/v2/representatives'
     extras = '&'.join("{!s}={!s}".format(i[0], i[1]) for i in filters)
     url = "%s?address=%s&key=%s&%s" % (url_base, address, API_KEY, extras)
 
-    # if oauth:
-    #     auth = OAuth1(API_KEY, API_SECRET, CLIENT_ID, CLIENT_SECRET)
-    #     response = requests.get(url, auth=auth)
-
-    # else:
     response = requests.get(url)
-    print(url)
-    print(response)
     return(response)
 
 
@@ -90,24 +101,32 @@ def extract_jurisdictions(rep_rec):
             jurisdictions.append(office_text)
         return(jurisdictions)
     except Exception as e:
+        logging.error('############## Error!!!!!!!!!!!!!!!!! ###########')
         logging.error('Error: %s' % rep_rec, exc_info=e)
+        global error_count
+        error_count += 1
 
 
 if __name__ == "__main__":
 
-    # Create timestamped folders
     now = datetime.now
+    start = now()
+
+    # Create timestamped folders
     time_of_run = '{}'.format(now().strftime('%Y-%m-%d-%H%M%S'))
     output_folder = output_folder + time_of_run + '/'
-    mkdir(output_folder)
+    os.mkdir(output_folder)
     log_file = output_folder + 'output.log'
     logging.basicConfig(filename=log_file, level=logging_level)
+    logging.info('----------------------------------------------------- NEW RUN -------------------------------------------')
+    logging.info(start)
+
 
     # Read and process data
     data = read_data(source_data)
     rep_records = []
     count = 1
-    for rec in data:
+    for rec in data[:500]:
         address = get_address(rec)
 
         # Must be a tuple, because each [0] item can have multiple instances.
@@ -121,31 +140,47 @@ if __name__ == "__main__":
         jurisdictions = extract_jurisdictions(rep_rec)
         signature = create_signature(rec, address)
 
-
         # Write lines out to files in folder
         try:
             for j in jurisdictions:
                 file_name = output_folder + j + '.txt'
-                f = open(file_name, "a+")
+
+                if os.path.isfile(file_name):
+                    f = open(file_name, "a+")
+                else:
+                    f = open(file_name, "a+")
+                    f.write(header_text)
+
                 f.write(signature)
                 f.write('\n')
                 f.close()
-        except:
-            print('############### Error!!!!!!!!!!!!!!!!! ############')
+        except Exception as e:
+            logging.error('############## Error!!!!!!!!!!!!!!!!! ###########')
+            logging.error('address: %s' % address)
+            logging.error('%s, %s' % (rep_rec, rep_rec.url))
+            logging.error('Error: %s' % rep_rec, exc_info=e)
+            error_count += 1
 
         count += 1
-        print(count)
-        # # Pulling back on API, to try to reduce errors.
-        #
-        # if count % 25 == 0:
-        #     time.sleep(1)
 
+        # Pulling back on API, to try to reduce errors.
+        if count % 50 == 0:
+            print(count)
+            print(error_count)
+            time.sleep(3)
+
+    end = datetime.now()
+    logging.info(end)
+    time_diff = end - start
+    logging.info(time_diff)
+    logging.info((time_diff.min, time_diff.seconds))
 
 
 #TODOs
 
 # - Check for duplicate signatures
 # - Check logs for requests and resulted in errors
+# - Add message to start of file
 # - Write tests
 
 #Resources
